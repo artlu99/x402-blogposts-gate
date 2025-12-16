ebb441190272248fc726db058791c217b68e7ffe
artlu99 <alu99.sm@gmail.com>
2025-12-16 19:56:10 +0100

    strip dependencies

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from "bun:test";
 import app from "./index";
 
 
 describe("Test the application", () => {
   it("Should return 200 response", async () => {
-    const res = await app.fetch(new Request("http://localhost/health"));
+        const res = await app.fetch(new Request("http://localhost/health"));
     expect(res.status).toBe(200);
-    expect(await res.json()).toEqual({ uptime: 42069 });
+    expect(await res.json()).toEqual(expect.objectContaining({ uptime: 42069 }));
   });
 });
